c68adf42e5a327a5d6474132f95995145a36f07f
Tomcat9761 <shoufei9761@163.com>
2019-11-25 17:33:04 +0800

    second commit about Login.vue and make some decoration

--- magic-vue/config/index.js
+++ magic-vue/config/index.js
@@ -11,7 +11,7 @@ module.exports = {
     assetsSubDirectory: 'static',
     assetsPublicPath: '/',
     proxyTable: {
-      'api': {
+      '/api': {
         target: 'http://localhost:8888',
         changeOrigin: true,
         pathRewrite: {
@@ -19,7 +19,6 @@ module.exports = {
         }
       }
     },
-
     // Various Dev Server settings
     host: 'localhost', // can be overwritten by process.env.HOST
     port: 8099, // can be overwritten by process.env.PORT, if port is in use, a free one will be determined
--- magic-vue/src/router/index.js
+++ magic-vue/src/router/index.js
@@ -1,15 +1,15 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 // 导入组件
-import Login from '../components/Login'
-import AppIndex from '../components/home/AppIndex'
+import Login from '@/components/Login'
+import AppIndex from '@/components/home/AppIndex'
 
 Vue.use(Router)
 
 export default new Router({
   routes: [
     {
-      path: '/login',
+      path: '/',
       name: 'Login',
       component: Login
     },
